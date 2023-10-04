f6f97804011d65a91b321f6f64c6449ec47ff69c
Nuthelk <nuthelkcd@gmail.com>
2023-10-04 11:22:06 -0500

    first commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,17 @@
+/* eslint-disable @next/next/no-sync-scripts */
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script src="html2pdf.bundle.min.js"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
+
     </Html>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,127 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React, { useEffect, useState } from 'react';
+import Draggable from './components/Draggable';
+import { DndContext } from '@dnd-kit/core';
+import Droppable from './components/Droppable';
 
-const inter = Inter({ subsets: ['latin'] })
+const Home = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [images, setImages] = useState<string[]>([]);
+  const [coordenadas, setCoordenadas] = useState({ x: 0, y: 0 });
+  const [elements, setElements] = useState<React.JSX.Element[]>([]);
 
-export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+
+
+  const readFileData = (file: File) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        resolve(e?.target?.result);
+      };
+      reader.onerror = (err) => {
+        reject(err);
+      };
+      reader.readAsDataURL(file);
+    });
+  };
+
+  const convertPdfToImages = async (file: File) => {
+    const PDFJS = require("pdfjs-dist/webpack");
+    const images = [];
+    const data = await readFileData(file);
+    const pdf = await PDFJS.getDocument(data).promise;
+    const canvas = document.createElement("canvas");
+    for (let i = 0; i < pdf.numPages; i++) {
+      const page = await pdf.getPage(i + 1);
+      const viewport = page.getViewport({ scale: 1.5 });
+      const context = canvas.getContext("2d");
+      canvas.height = viewport.height;
+      canvas.width = viewport.width;
+      await page.render({ canvasContext: context, viewport: viewport }).promise;
+      images.push(canvas.toDataURL());
+    }
+    canvas.remove();
+    return images;
+  };
+
+  const loadPdfContent = async () => {
+    if (selectedFile) {
+      const convertedImages = await convertPdfToImages(selectedFile);
+      setImages(convertedImages);
+      // Aquí puedes usar convertedImages para mostrar las imágenes en algún lugar
+    }
+  };
+
+  const uploadImg = (e: any) => {
+    const file = e.target.files[0];
+    setSelectedFile(file);
+  };
+
+  function handleDragEnd(event: any) {
+    const element = document.getElementById("elemento");
+    setCoordenadas(element?.getBoundingClientRect()!);
+    const newStyle = `absolute,top-[${event?.over?.rect?.top}],bottom-[${event?.over?.rect?.bottom}],left-[${event?.over?.rect?.left}],right-[${event?.over?.rect?.right}],z-50`;
+  
+    if (element instanceof HTMLElement && event?.over?.rect) {
+      const clonedElement = (
+        <div
+          key={`elemento-copia-${Date.now()}`}
+          className={`p-2 bg-blue-600 max-w-min text-white font-semibold text-sm rounded-lg ${newStyle}`}
+        >
+          {element.textContent}
         </div>
+      );
+      setElements([...elements, clonedElement]);
+    }
+  }
+
+console.log(elements)
+
+  return (
+    <div>
+      <input
+        type="file"
+        id="fileInput"
+        accept=".pdf, .docx, .jpg"
+        onChange={uploadImg}
+      />
+      <button onClick={loadPdfContent}>Cargar PDF y Convertir a Imágenes</button>
+      <div className='mt-10'>
+        <DndContext onDragEnd={handleDragEnd}>
+          <div className=' flex items-start gap-14'>
+            <div className='h-[800px] flex flex-col gap-10 p-12 border '>
+              <Draggable id={1} >
+                <div id='elemento' className='p-2 bg-blue-600 text-white font-semibold text-sm rounded-lg' >
+                  Name
+                </div>
+              </Draggable>
+              <Draggable id={4} >
+                <div id='elemento' className='p-2 bg-blue-600 text-white font-semibold text-sm rounded-lg' >
+                  lastName
+                </div>
+              </Draggable>
+            </div>
+
+            <div></div>
+
+            <Droppable >
+              <div className='flex flex-col gap-10 bg-white px-12 max-h-[800px] max-w-[900px] overflow-y-auto '>
+                {images.map((image, index) => (
+                  <div key={index} className=' ' >
+                    <img src={image} alt={`Page ${index + 1}`} className='' />
+                  </div>                    
+                ))}
+                {
+                    elements.map((element, index) => (
+                      <div key={index}>{element}</div>
+                    ))
+                  }
+              </div>
+            </Droppable>
+          </div>
+        </DndContext>
       </div>
+    </div>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Home;
